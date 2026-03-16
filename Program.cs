13d5920a5cd3c74e88e8c42927f54a1be4074c56
sanjap14 <123950520+sanjap14@users.noreply.github.com>
2026-03-16 11:47:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon39_3_16_2026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Andrej Onic
+            
+        }
+    }
+}
