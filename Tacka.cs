a1aa45fa2010fc8a0323c63c601e1b0fe8ac4d11
sanjap14 <123950520+sanjap14@users.noreply.github.com>
2026-03-16 12:26:31 +0100

    verzija 1

---
+++ Tacka.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon39_3_16_2026
+{
+    class Tacka
+    {
+        public double x, y;
+        public Tacka()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Tacka(double a, double b)
+        {
+            x = a;
+            y = b;
+        }
+        public double d()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
